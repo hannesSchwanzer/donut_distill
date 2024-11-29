ba12fade369fc5b75d482c0ae7874a17d2461a8b
Hannes Schwanzer <hannes.schwanzer@student.hs-rm.de>
2024-11-29 16:09:51 +0100

    feat: CLI for finetuned model

---
+++ donut_cli.py
@@ -0,0 +1,56 @@
+from transformers import DonutProcessor, VisionEncoderDecoderModel
+import os
+from PIL import Image
+import torch
+import re
+import json
+
+if __name__ == "__main__":
+    print("Enter path to model directory:")
+    model_path = input()
+    if not os.path.isdir(model_path):
+        print("Path is not a directory")
+        exit(1)
+
+    processor = DonutProcessor.from_pretrained(model_path)
+    model = VisionEncoderDecoderModel.from_pretrained(model_path)
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model.to(device)
+    task_prompt = ""
+
+    while True:
+        print("Enter path to document image:")
+        document_path = input()
+        try:
+            image = Image.open(document_path)
+        except:
+            print("Couldn't open image")
+            continue
+
+        pixel_values = processor(image, return_tensors="pt").pixel_values
+        decoder_input_ids = processor.tokenizer(
+            task_prompt, add_special_tokens=False, return_tensors="pt"
+        )["input_ids"]
+        outputs = model.generate(
+            pixel_values.to(device),
+            decoder_input_ids=decoder_input_ids.to(device),
+            max_length=model.decoder.config.max_position_embeddings,
+            early_stopping=True,
+            pad_token_id=processor.tokenizer.pad_token_id,
+            eos_token_id=processor.tokenizer.eos_token_id,
+            use_cache=True,
+            num_beams=1,
+            bad_words_ids=[[processor.tokenizer.unk_token_id]],
+            return_dict_in_generate=True,
+            output_scores=True,
+        )
+        sequence = processor.batch_decode(outputs.sequences)[0]
+        sequence = sequence.replace(processor.tokenizer.eos_token, "").replace(
+            processor.tokenizer.pad_token, ""
+        )
+        sequence = re.sub(
+            r"<.*?>", "", sequence, count=1
+        ).strip()  # remove first task start token
+
+        json_sequence = processor.token2json(sequence)
+        print(json.dumps(json.loads(json_sequence), indent=2))
