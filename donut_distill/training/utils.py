fix: adjusted scheduler for gradient accumulation

donut_distill/training/utils.py
@@ -108,6 +108,7 @@ def prepare_optimizer_and_scheduler(model: VisionEncoderDecoderModel, len_traini
         max_iter = (CONFIG.MAX_EPOCHS * len_trainingsdata) / (
             CONFIG.TRAIN_BATCH_SIZES * torch.cuda.device_count() * CONFIG.NUM_NODES
         )
+        max_iter = max_iter // CONFIG.ACCUMULATION_STEPS  # Adjust for gradient accumulation
 
     # If max training steps are defined, ensure we do not exceed them
     if int(CONFIG.MAX_STEPS) > 0:
@@ -119,6 +120,9 @@ def prepare_optimizer_and_scheduler(model: VisionEncoderDecoderModel, len_traini
 
     assert max_iter is not None, "max_iter must be defined before creating the scheduler."
 
-    scheduler = cosine_scheduler(optimizer, max_iter, CONFIG.WARMUP_STEPS)
+    # Adjust warmup steps for gradient accumulation
+    warmup_steps = CONFIG.WARMUP_STEPS // CONFIG.ACCUMULATION_STEPS
+
+    scheduler = cosine_scheduler(optimizer, max_iter, warmup_steps)
     
     return optimizer, scheduler